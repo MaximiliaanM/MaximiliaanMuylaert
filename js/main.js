100e5435161013995e491e99d056894259ab4270
MaximiliaanM <maximiliaan.muylaert@student.hogent.be>
2020-04-17 18:50:32 +0200

    update

--- js/main.js
+++ js/main.js
@@ -101,17 +101,23 @@
 		});
 	}
 
-	/*--/ Testimonials owl /--*/
-	$('#testimonial-mf').owlCarousel({
-		margin: 20,
-		autoplay: true,
-		autoplayTimeout: 4000,
-		autoplayHoverPause: true,
-		responsive: {
-			0: {
-				items: 1,
+	/*--/ Magnific popup /--*/
+		$('.image-popup-no-margins').magnificPopup({
+			type: 'image',
+			closeOnContentClick: true,
+			closeBtnInside: false,
+			fixedContentPos: true,
+			mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+			image: {
+				verticalFit: true
+			},
+			zoom: {
+				enabled: true,
+				duration: 300 // don't foget to change the duration also in CSS
+			},
+			gallery:{
+				enabled: false
 			}
-		}
-	});
+		});
 
 })(jQuery);
