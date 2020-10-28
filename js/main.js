0ee383d7282abde61f5724877b8d228e17a75da7
MaximiliaanM <maximiliaan.muylaert@student.hogent.be>
2020-10-28 23:19:59 +0100

    update info & clean code

--- js/main.js
+++ js/main.js
@@ -120,4 +120,14 @@
 			}
 		});
 
+	/*--/ Magnific popup /--*/
+		
+    function calculate_age(dob) {
+      var diff_ms = Date.now() - dob.getTime();
+      var age_dt = new Date(diff_ms);
+      return Math.abs(age_dt.getUTCFullYear() - 1970);
+    }
+    document.getElementById("ageJavascript").innerHTML = calculate_age(new Date(1999, 5, 21));
+      
+
 })(jQuery);
